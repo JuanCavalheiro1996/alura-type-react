7537ff1d9e9ee52f2830e104d5ab2fc42bc841e6
JuanCavalheiro1996 <JuanCavalheiroOliveira@gmail.com>
2023-10-04 20:40:46 -0300

    New commit

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,15 +1,25 @@
 import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
 export default function Item({
   tarefa,
   tempo,
   selecionado,
   completado,
   id,
-}: ITarefa) {
+  selecionaTarefa,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`}
+      onClick={() =>
+        selecionaTarefa({ tarefa, tempo, selecionado, completado, id })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
