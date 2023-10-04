9ce2e23a031100569af2a4ab020b5d4ac67ad99b
JuanCavalheiro1996 <JuanCavalheiroOliveira@gmail.com>
2023-10-03 22:16:32 -0300

    Commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren } from "react";
+import style from "./Button.module.scss";
+
+class Button extends React.Component<PropsWithChildren> {
+  render() {
+    return <button className={style.botao}>{this.props.children}</button>;
+  }
+}
+
+export default Button;
